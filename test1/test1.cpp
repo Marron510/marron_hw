f4ff67100e03070f8f372f12a17fcd93f48439d3
Marron0510 <wkdtl345@naver.com>
2025-02-21 18:04:59 +0900

    업데이트

---
+++ test1/test1.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+
+
+class a
+{
+public:
+	a()
+	{
+		std::cout << "a";
+		function();
+	}
+	virtual void function()
+	{
+		std::cout << "af";
+	}
+
+};
+
+class b : public a
+{
+public:
+	b()
+	{
+		std::cout << "b";
+		function();
+	}
+	void function() override
+	{
+		std::cout << "bf";
+	}
+};
+
+int main()
+{
+
+	b newb = b();
+	
+}
+
